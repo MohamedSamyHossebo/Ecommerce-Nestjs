d74c570073c6f39316674e30e1ac14909d26e8e7
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-07-02 01:23:23 +0300

    Enhance database connection setup with Mongoose integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { resolve } from 'path';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: resolve('./config/.env.dev'),
       isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('DB_URI'),
+        dbName: configService.get<string>('DB_NAME'),
+        onConnectionCreate(connection: Connection) {
+          connection.on('connected', () => {
+            console.log(
+              `${configService.get<string>('DB_NAME')} connection established successfully on ${configService.get<string>('DB_URI')}`,
+            );
+          });
+        },
+      }),
     }),
   ],
   controllers: [AppController],
