faa8b2839d39af3d3755aa51ef97a56228727c62
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-07-09 12:55:00 +0300

    feat: add product Mongoose schema and model configuration

--- src/DB/Models/product.model.ts
+++ src/DB/Models/product.model.ts
@@ -17,11 +17,17 @@ export class Product {
   @Prop({ type: Number, required: true })
   price!: number;
 
+  @Prop({ type: Number, default: 0 })
+  discountPrice!: number;
+
   @Prop({ type: Number, required: true })
   stock!: number;
 
-  @Prop({ type: String, trim: true })
-  overview!: string;
+  @Prop({ type: Number, default: 0 })
+  raiting!: number;
+
+  @Prop({ type: Number, default: 0 })
+  numReviews!: number;
 
   @Prop({
     type: mongoose.Schema.Types.ObjectId,
