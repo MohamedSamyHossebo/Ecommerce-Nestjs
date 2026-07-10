b533f381b169de73325240b8af86cbfcd6626ae7
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-07-10 21:18:41 +0300

    feat: add Coupon database model with schema validation and relations

---
+++ src/DB/Models/coupons.model.ts
@@ -0,0 +1,45 @@
+import { MongooseModule, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+
+@Schema({
+  timestamps: true,
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true },
+})
+export class Coupon {
+  @Prop({ required: true, unique: true, trim: true, uppercase: true })
+  code!: string;
+
+  @Prop({ required: true, type: Number, min: 1, max: 100 })
+  discountPercentage!: number;
+
+  @Prop({ required: true, type: Date })
+  expireDate!: Date;
+
+  @Prop({ required: true, type: Number, min: 1 })
+  maxUsage!: number;
+
+  @Prop({ required: true, type: Number, min: 0 })
+  usedCount!: number;
+
+  @Prop({ required: true })
+  isActive!: boolean;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    default: [],
+  })
+  usedBy!: mongoose.Types.ObjectId[];
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  createdBy!: mongoose.Types.ObjectId;
+}
+export const CouponSchema = SchemaFactory.createForClass(Coupon);
+export type HCouponDocument = HydratedDocument<Coupon>;
+export const CouponModel = MongooseModule.forFeature([
+  { name: Coupon.name, schema: CouponSchema },
+]);
