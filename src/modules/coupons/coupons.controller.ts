da512240ab734b10f50c1499eeb2344d52ddefbf
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-07-10 21:44:34 +0300

    feat: implement CouponsController with CRUD and validation endpoints

--- src/modules/coupons/coupons.controller.ts
+++ src/modules/coupons/coupons.controller.ts
@@ -17,7 +17,27 @@ export class CouponsController {
   constructor(private readonly couponsService: CouponsService) {}
 
   @Post()
-  create(@Body() createCouponDto: CreateCouponDto, @Req() req) {
+  create(@Body() createCouponDto: CreateCouponDto, @Req() req: any) {
     return this.couponsService.create(createCouponDto, req.user._id as string);
+  }
+
+  @Get()
+  findAll() {
+    return this.couponsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.couponsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCouponDto: UpdateCouponDto) {
+    return this.couponsService.update(id, updateCouponDto);
+  }
+
+  @Post('validate')
+  validateCoupon(@Body('code') code: string, @Req() req: any) {
+    return this.couponsService.validateCoupone(code, req.user._id as string);
   }
 }
