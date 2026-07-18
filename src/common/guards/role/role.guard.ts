4e34281ba96a8f2eea820ffaec64a12c6ed1b39a
MohamedSamyHossebo <eng.mohamedsamyhossebo@gmail.com>
2026-07-18 20:23:17 +0300

    feat: implement role-based access control guard for HTTP and GraphQL contexts

--- src/common/guards/role/role.guard.ts
+++ src/common/guards/role/role.guard.ts
@@ -6,13 +6,22 @@ import {
   UnauthorizedException,
   mixin,
 } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
 import { UserRoleEnum } from 'src/common/enums/user.enum';
 
 export const RoleGuard = (...roles: UserRoleEnum[]) => {
   @Injectable()
   class RoleGuardMixin implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
-      const request = context.switchToHttp().getRequest();
+      const contextType = context.getType<GqlContextType>();
+      let request: any;
+
+      if (contextType === 'graphql') {
+        request = GqlExecutionContext.create(context).getContext().req;
+      } else {
+        request = context.switchToHttp().getRequest();
+      }
+
       const user = request.user;
 
       if (!user) {
